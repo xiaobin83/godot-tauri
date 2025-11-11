41f5bba79cd0a4f74e66b0ff124d9318d673924f
Xiaobin <xiaobin@tal.com>
2025-11-11 17:28:39 +0800

    connect tauri and godot

--- src/App.vue
+++ src/App.vue
@@ -11,28 +11,21 @@ async function greet() {
 }
 </script>
 
+<script lang="ts">
+export default {
+  data() {
+    return {
+      subPagePath: '/public/godot/PlayGround.html'
+    };
+  }
+};
+</script>
+
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
-
-    <div class="row">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
+	<div>
+		<iframe :src="subPagePath" width="100%" height="500px" frameborder="0"></iframe>
+	</div>
   </main>
 </template>
 
